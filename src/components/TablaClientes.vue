5bf284b86abdef793a2fc99ae6c392ab3cc4fb12
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2024-01-27 18:19:30 +0100

    Update TablaClientes.vue

--- src/components/TablaClientes.vue
+++ src/components/TablaClientes.vue
@@ -108,7 +108,8 @@ export default {
         try {
         // Ahora hacemos una solicitud directamente al servidor JSON
         // const response = await fetch('http://localhost:3000/clientes'); 
-        const response = await fetch('https://github.com/XoanCarlos/myapp/blob/main/public/data/clientes.json'):
+            
+        const response = await fetch('https://raw.githubusercontent.com/XoanCarlos/myapp/master/clientes.json'):
         // Cambia el puerto si es diferente
 
         if (!response.ok) {
