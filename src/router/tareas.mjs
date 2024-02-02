523ed04f8104b4d0dd8b3cfe73698003de94caa1
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2024-02-02 11:30:19 +0100

    error en post con postman

--- src/router/tareas.mjs
+++ src/router/tareas.mjs
@@ -1,13 +1,37 @@
 import express from 'express';
 const rutas = express.Router();
 
+import tarea from '../models/tarea.mjs';
 
 //otra forma de crear rutas
 
-rutas.get('/', (req, res) => {
-    res.send('Ruta de las tareas');
+rutas.get('/', async(req, res) => {
+    const tareas = await tarea.find();    
+    res.json(tareas);
   });
-  
 
+rutas.post('/', async(req, res) => {
+  try {
+    const nuevatarea = new tarea();
+    
+    nuevatarea.nombre = req.body.nombre;
+    nuevatarea.descripcion = req.body.descripcion;
+    nuevatarea.fecha = req.body.fecha;
+    nuevatarea.prioridad = req.body.prioridad;
+    
+      await nuevatarea.save();
+      console.log(nuevatarea);
+      res.json({
+        status: 'Tarea guardada'
+      
+    });
+  } catch (error) {
+  console.error('Error al guardar la tarea:', error);
+  res.status(500).json({
+      error: 'Error al guardar la tarea'
+  });
+} 
+});
+  
 export default rutas;
   
